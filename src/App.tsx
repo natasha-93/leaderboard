29721f8e7d42909f15e3012175ba5a29a7f41e23
Natasha Lawson <natasha.lawson93@gmail.com>
2020-04-29 10:43:37 -0600

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [people, setPeople] = useState([
+    "Bob",
+    "Fred",
+    "Hazel",
+    "Ash",
+    "Ben",
+    "Lewis",
+  ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Leaderboard</h2>
+      <ol>
+        {people.map((person, index) => {
+          return (
+            <li key={index}>
+              <button
+                disabled={index === 0}
+                onClick={(e) => {
+                  const newPeople = [
+                    person,
+                    ...people.slice(0, index),
+                    ...people.slice(index + 1),
+                  ];
+                  setPeople(newPeople);
+                }}
+              >
+                ⬆⬆
+              </button>
+              <button
+                disabled={index === 0}
+                onClick={(e) => {
+                  const newPeople = [
+                    ...people.slice(0, index - 1),
+                    person,
+                    people[index - 1],
+                    ...people.slice(index + 1),
+                  ];
+                  setPeople(newPeople);
+                }}
+              >
+                ⬆
+              </button>
+              <button
+                disabled={index === people.length - 1}
+                onClick={(e) => {
+                  const newPeople = [
+                    ...people.slice(0, index),
+                    people[index + 1],
+                    person,
+                    ...people.slice(index + 2),
+                  ];
+                  setPeople(newPeople);
+                }}
+              >
+                ⬇
+              </button>
+              <button
+                disabled={index === people.length - 1}
+                onClick={(e) => {
+                  const newPeople = [
+                    ...people.slice(0, index),
+                    ...people.slice(index + 1),
+                    person,
+                  ];
+                  setPeople(newPeople);
+                }}
+              >
+                ⬇⬇
+              </button>
+              {person}
+            </li>
+          );
+        })}
+      </ol>
     </div>
   );
 }
